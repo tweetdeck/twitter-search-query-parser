6ebe43cf3a11516c1f50ec7ed1a0324c37f0b8dc
Tom Ashworth <tashworth@twitter.com>
2016-06-21 11:55:09 +0100

    Output data structure (no OR)

--- try.js
+++ try.js
@@ -1,9 +1,75 @@
-const types = {};
+const types = {
+  Container: {
+    reduce: function () {
+      return this.value.reduce();
+    }
+  },
+  Query: {
+    reduce: function () {
+      return [
+        'Query',
+        this.elements.map(elem => elem.operator.reduce())
+      ];
+    }
+  },
+  Or: {
+    reduce: function () {
+      console.log('OR', this);
+      return [
+        'Or',
+        this.left.reduce(),
+        this.right.reduce()
+      ];
+    }
+  },
+  Including: {
+    reduce: function () {
+      return [
+        'Including',
+        this.text
+      ];
+    }
+  },
+  Excluding: {
+    reduce: function () {
+      return [
+        'Excluding',
+        this.word.text
+      ];
+    }
+  },
+  KV: {
+    reduce: function () {
+      return [
+        'KV',
+        this.k.text,
+        this.v.text
+      ];
+    }
+  },
+  IsQuestion: {
+    reduce: function () {
+      return [
+        'IsQuestion',
+        true
+      ];
+    }
+  }
+};
 const tree = require('./query').parse(process.argv[2], { types });
 
 console.log(
   require('util').inspect(
     tree,
+    { depth: null, colors: true }
+  )
+);
+
+console.log();
+
+console.log(
+  require('util').inspect(
+    tree.reduce(),
     { depth: null, colors: true }
   )
 );
