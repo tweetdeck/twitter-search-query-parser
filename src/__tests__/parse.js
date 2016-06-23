42d35a118324339bae590d234297752ac451f10e
Tom Ashworth <tashworth@twitter.com>
2016-06-23 12:21:17 +0100

    Generalise pairs and make then in/excludable

--- src/__tests__/parse.js
+++ src/__tests__/parse.js
@@ -22,6 +22,33 @@ const testCases = [
     ]
   ],
   [
+    'pairs',
+    `filter:vine exclude:retweets min_replies:100 lang:es to:jack since:2016-01-01
+    -filter:vine -exclude:retweets -min_replies:100 -lang:es -to:jack -since:2016-01-01`,
+    [
+      ['Including', ['Pair', 'filter', 'vine']],
+      ['Including', ['Pair', 'exclude', 'retweets']],
+      ['Including', ['Pair', 'min_replies', '100']],
+      ['Including', ['Pair', 'lang', 'es']],
+      ['Including', ['Pair', 'to', 'jack']],
+      ['Including', ['Pair', 'since', '2016-01-01']],
+      ['Excluding', ['Pair', 'filter', 'vine']],
+      ['Excluding', ['Pair', 'exclude', 'retweets']],
+      ['Excluding', ['Pair', 'min_replies', '100']],
+      ['Excluding', ['Pair', 'lang', 'es']],
+      ['Excluding', ['Pair', 'to', 'jack']],
+      ['Excluding', ['Pair', 'since', '2016-01-01']]
+    ]
+  ],
+  [
+    'list',
+    'list:beep/boop -list:beep/boop',
+    [
+      ['Including', ['List', 'beep', 'boop']],
+      ['Excluding', ['List', 'beep', 'boop']]
+    ]
+  ],
+  [
     'extreme example',
     `search #search @search -query filter:vine exclude:retweets exclude:nativeretweets
      min_replies:10 OR min_retweets:100 min_faves:20 lang:es OR to:jack ?
@@ -33,17 +60,29 @@ const testCases = [
       ['Including', ['Text', '#search']],
       ['Including', ['Text', '@search']],
       ['Excluding', ['Text', 'query']],
-      ['Pair', 'filter', 'vine'],
-      ['Pair', 'exclude', 'retweets'],
-      ['Pair', 'exclude', 'nativeretweets'],
-      ['Or', [['Pair', 'min_replies', '10'], ['Pair', 'min_retweets', '100']]],
-      ['Pair', 'min_faves', '20'],
-      ['Or', [['Pair', 'lang', 'es'], ['Pair', 'to', 'jack']]],
+      ['Including', ['Pair', 'filter', 'vine']],
+      ['Including', ['Pair', 'exclude', 'retweets']],
+      ['Including', ['Pair', 'exclude', 'nativeretweets']],
+      [
+        'Or',
+        [
+          ['Including', ['Pair', 'min_replies', '10']],
+          ['Including', ['Pair', 'min_retweets', '100']]
+        ]
+      ],
+      ['Including', ['Pair', 'min_faves', '20']],
+      [
+        'Or',
+        [
+          ['Including', ['Pair', 'lang', 'es']],
+          ['Including', ['Pair', 'to', 'jack']]
+        ]
+      ],
       ['IsQuestion', true],
-      ['Pair', 'since', '2016-01-01'],
-      ['Pair', 'until', '2016-02-01'],
-      ['List', 'NASA', 'astronauts-in-space-now'],
-      ['Pair', 'filter', 'verified'],
+      ['Including', ['Pair', 'since', '2016-01-01']],
+      ['Including', ['Pair', 'until', '2016-02-01']],
+      ['Including', ['List', 'NASA', 'astronauts-in-space-now']],
+      ['Including', ['Pair', 'filter', 'verified']],
       [
         'Or',
         [
